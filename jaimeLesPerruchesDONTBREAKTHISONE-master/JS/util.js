7aa5f8a94bbdc0d447fbd58a731a1ec16ef5d1fd
MathieuDude <mlabelle1999@hotmail.com>
2019-12-07 23:51:48 -0500

    Delete util.js

--- jaimeLesPerruchesDONTBREAKTHISONE-master/JS/util.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    var threadCount = 10;
-        $("#thread-srch-btn").click(function(){
-            threadCount = threadCount + 10;
-            $("#thread-container").load("threadlist.dom.php", {
-                threadNewCount: threadCount
-        });
-    });
-});
