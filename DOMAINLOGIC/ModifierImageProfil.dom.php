58f4a543b8cc36cf54f8809d4438aee458cdcb99
Edixz1 <stefgenereux@hotmail.com>
2019-12-19 17:07:34 -0500

    SA MARCHE CALISS TOUT LE PROFIL MARCHE

--- DOMAINLOGIC/ModifierImageProfil.dom.php
+++ DOMAINLOGIC/ModifierImageProfil.dom.php
@@ -3,17 +3,18 @@
 include_once __DIR__ . "/../CLASSES/MEDIA/media.php";
 
 session_start();
-
-if(isset($_FILES['Media']) && !empty($_POST['Name'])){
- 
-    $title = $_POST['Name'];
     $target_dir = "MEDIA/";
 
-    //obtenir l'extention du fichier uploader+
-    $media_file_type = pathinfo($_FILES['Media']['name'] ,PATHINFO_EXTENSION);
-    //$media_file_ext = strtolower(end(explode('.',$_FILES['Media']['name'])));
-  
-    // Valid file extensions
+    $aUser = new User();
+
+    
+    //mets limage de profil dans un dossier dans media (le dossier cest PP) et le nome : $idUser 
+    $media_file_type = pathinfo($_FILES["newImg"]["name"],PATHINFO_EXTENSION);
+
+    //si on enleve ca ca explose
+   
+    var_dump($media_file_type) ;
+
     $img_extensions_arr = array("jpg","jpeg","png","gif");
     $vid_extensions_arr = array("webm", "avi", "wmv", "rm", "rmvb", "mp4", "mpeg");
 
@@ -21,33 +22,23 @@ if(isset($_FILES['Media']) && !empty($_POST['Name'])){
         $type = "image";
         echo "image";
     }
+    //ici quon modifie si on veut des video en image profile
     else if(in_array($media_file_type, $vid_extensions_arr)){
-        $type = "video";
-        echo "video";
+        echo "INVALID FILE TYPE VIDEO IS NOT ACCEPTING";
+        die();
     }
     else{
+        
+        
         echo "INVALID FILE TYPE";
         die();
     }
 
-    //creation d'un nom unique pour la "PATH" du fichier
-    $path = tempnam("../MEDIA", '');
-    unlink($path);
-    $file_name = basename($path, ".tmp");
-    
-    //creation de l'url pour la DB
-    $url = $target_dir . $file_name . "." . $media_file_type;
-    $file_url = $file_name . "." . $media_file_type;
-    
-    //deplacement du fichier uploader vers le bon repertoire (Medias)
-    move_uploaded_file($_FILES['Media']['tmp_name'], "../" . $url);
+    //le path du folder PP
+    $imagePath  =  "../MEDIA/PP/" .  $_SESSION["userEmail"] . "." .  $media_file_type;
+    move_uploaded_file($_FILES['newImg']['tmp_name'],$imagePath);
+    //mets le path de PP dans une session
 
-
-    //create entry in database
-    //MUST DEFINE ALBUMID
-    Media::create_entry($type, $file_url, $title, $_SESSION["lastVisitedAlbumID"], $_SESSION["userID"]);
-
-    //redirection
+    //validateLogin
     header("Location: ../billboard.php");
     die();
-}
